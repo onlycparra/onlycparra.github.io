1a7eb45ae82f2f8291b6723dd528b499b6ab0ead
Claudio Parra <parraca@uci.edu>
2022-11-03 02:38:05 -0700

    day 2 almost done

--- docs/2022/code_training/7q-each-day/589_alt.py
+++ docs/2022/code_training/7q-each-day/589_alt.py
@@ -9,4 +9,13 @@ class Node:
 class Solution:
     # Iterative Solution
     def preorder(self, root: 'Node') -> List[int]:
-        
+        if root == None:
+            return []
+        expansion_stack = [root]
+        preorder_list = []
+        while len(expansion_stack) > 0:
+            curr_node = expansion_stack.pop()
+            preorder_list.append(curr_node.val)
+            expansion_stack.extend(reversed(curr_node.children))
+            
+        return preorder_list
